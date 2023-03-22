176ef4411cacbfba9c4ff8607b86f01d9ff85148
spxedu <62041731+spxedu@users.noreply.github.com>
2023-03-22 18:15:39 +0700

    Upload file đơn giản

--- express-layout-upload/controllers/sanpham.controller.js
+++ express-layout-upload/controllers/sanpham.controller.js
@@ -1,8 +1,27 @@
+var fs = require('fs');
+
 exports.list = (req, res, next)=>{
 
     res.render('sanpham/list');
 }
 
 exports.add = (req, res, next)=>{
+
+    console.log(req.file, req.body);
+    if(req.method =='POST'){
+        // xử lý file upload
+        // di chuyển file từ thư mục tmp sang public/uploads
+        //fs.rename(đường dẫn gốc, đường dẫn mới, callback)
+        fs.rename(req.file.path,
+                 './public/uploads/'+ req.file.originalname,
+                 (err)=>{
+                    if(err)
+                        console.log(err);
+                    else{
+                        // không có lỗi, tạo url, bỏ chữ public/
+                    console.log("Url: http://localhost:3000/uploads/" +req.file.originalname );
+                    }
+                 }) 
+    }
     res.render('sanpham/add');
 }
--- express-layout-upload/routes/sanpham.js
+++ express-layout-upload/routes/sanpham.js
@@ -5,4 +5,9 @@ var spCtrl = require('../controllers/sanpham.controller');
 router.get('/', spCtrl.list);
 router.get('/add', spCtrl.add);
 
+
+var multer = require('multer');
+var objUpload = multer({ dest: './tmp' });
+router.post('/add',objUpload.single("file_anh"), spCtrl.add);
+
 module.exports = router;
