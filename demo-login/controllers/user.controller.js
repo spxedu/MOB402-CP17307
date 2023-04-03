c2425b0035bdb6dc6aa993d0a81eddaf0013b5ea
spxedu <62041731+spxedu@users.noreply.github.com>
2023-04-03 17:58:44 +0700

    Làm chức năng login

--- demo-login/controllers/user.controller.js
+++ demo-login/controllers/user.controller.js
@@ -1,7 +1,30 @@
 const myMD = require('../models/user.model');
 
-exports.Login = (req, res, next)=>{
+exports.Login = async (req, res, next)=>{
     let msg = '';
+    if(req.method == 'POST'){
+        try {
+            let objU = await myMD.userModel.findOne({username: req.body.username});
+            console.log(objU);
+            if(objU != null ){
+                // tồn tại username ==> kiểm tra passwd
+                if(objU.passwd == req.body.passwd){
+                    // đúng thông tin tài khoản ==> lưu vào session
+                    req.session.userLogin = objU; 
+                    // chuyển trang về trang quản trị
+                    return res.redirect('/users');
+                }else{
+                    msg = 'Sai password';
+                }
+
+            }else{
+                msg = 'Không tồn tại tài khoản: ' + req.body.username;
+            }
+
+        } catch (error) {
+            msg = error.message;
+        }
+    } 
 
     res.render('user/login', {msg:msg})
 }
