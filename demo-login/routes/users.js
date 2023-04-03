3869c76542f41952ba83c86a73f1187110f7aa1c
spxedu <62041731+spxedu@users.noreply.github.com>
2023-04-03 18:20:25 +0700

    Thêm chức năng middleware validate login

--- demo-login/routes/users.js
+++ demo-login/routes/users.js
@@ -1,20 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var userCtrl = require('../controllers/user.controller');
+var check_login = require('../middleware/check_login');
+
+// middleware cho toàn bộ các router phía dưới
+router.use( (req, res, next)=>{
+    console.log("---> Đã gọi middleware .....");
+    next();
+});
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', check_login.yeu_cau_login ,function(req, res, next) {
   res.send('respond with a resource');
 });
 
 // route login
-router.get('/login',userCtrl.Login);
-router.post('/login',userCtrl.Login);
+router.get('/login', check_login.khong_yc_login ,userCtrl.Login);
+router.post('/login',check_login.khong_yc_login ,userCtrl.Login);
 
-router.get('/reg',userCtrl.Reg);
-router.post('/reg',userCtrl.Reg);
+router.get('/reg',check_login.khong_yc_login ,userCtrl.Reg);
+router.post('/reg',check_login.khong_yc_login ,userCtrl.Reg);
 
-router.get('/logout',userCtrl.Logout);
+router.get('/logout',check_login.yeu_cau_login,userCtrl.Logout);
 
 
 
